8cd7c436e83538767ed654abbf2ff7ec8ebe860a
Anser TP <ansertp47@gmail.com>
2024-02-22 19:25:54 +0530

    Committing changes to pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,7 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:const Expenses(),
+      home: const Expenses(),
     ),
   );
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//     );
-//   }
-// }
